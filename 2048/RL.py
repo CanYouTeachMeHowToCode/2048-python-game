e00ac31de70172be4835df1a56865c9a96339342
吴轶伦 <wuyilun@wuyilundeMacBook-Pro.local>
2021-12-24 23:52:27 -0500

    finish RL model and training process code

--- 2048/RL.py
+++ 2048/RL.py
@@ -14,17 +14,19 @@ from board import Board
 # Reference: https://github.com/navjindervirdee/2048-deep-reinforcement-learning
 # for deep reinforcement learning: currently only consider board with size 4 (can be improved later)
 
-# convert the input game board into corresponding power of 2 tensor.
+# convert the input game board (or batch of game boards) into corresponding power of 2 tensor.
 def convertBoard2PowBoard(board):
-    powerBoard = np.zeros(shape=(1, 16, len(board), len(board)), dtype=np.float32) # 16 is for board size 4 (2^0 to 2^15)
-    for i in range(len(board)):
-        for j in range(len(board)):
-            if not board[i][j]:
-                powerBoard[0][0][i][j] = 1.0
-            else:
-                power = int(math.log(board[i][j],2))
-                powerBoard[0][power][i][j] = 1.0
-    return powerBoard # NCHW format, different from NHWC in tf
+    batchSize = board.size()[0]
+    powerBoard = np.zeros(shape=(batchSize, 16, board.size()[1], board.size()[2]), dtype=np.float32) # 16 is for board size 4 (2^0 to 2^15)
+    for batchIndex in range(batchSize):
+        for i in range(board.size()[1]):
+            for j in range(board.size()[2]):
+                if not board[batchIndex][i][j]:
+                    powerBoard[batchIndex][0][i][j] = 1.0
+                else:
+                    power = int(math.log(board[batchIndex][i][j],2))
+                    powerBoard[batchIndex][power][i][j] = 1.0
+    return torch.from_numpy(powerBoard) # NCHW format, different from NHWC in tf
 
 # find the number of empty tiles in the game board.
 def findEmptyTiles(board):
@@ -110,83 +112,88 @@ policyNet = MyPolicy()
 policyNet.apply(initWeightBias)
 print("policyNet", policyNet)
 
-# unit test
-GameBoard = Board(4)
-print(GameBoard.board)
-print(convertBoard2PowBoard(GameBoard.board))
-GameBoard.moveLeft()
-print(GameBoard.board)
-print(convertBoard2PowBoard(GameBoard.board))
-GameBoard.addNewTile()
-print(GameBoard.board)
-print(convertBoard2PowBoard(GameBoard.board))
+# # unit test
+# GameBoard = Board(4)
+# GameBoard.printBoard()
+# print("asd", torch.from_numpy(np.expand_dims(GameBoard.board, axis=0)))
+# print(convertBoard2PowBoard(torch.from_numpy(np.expand_dims(GameBoard.board, axis=0))))
+# GameBoard.moveLeft()
+# GameBoard.printBoard()
+# print(convertBoard2PowBoard(torch.from_numpy(np.expand_dims(GameBoard.board, axis=0))))
+# GameBoard.addNewTile()
+# GameBoard.printBoard()
+# print(convertBoard2PowBoard(torch.from_numpy(np.expand_dims(GameBoard.board, axis=0))))
+# print("GameBoard.board", GameBoard.board)
 
-print("GameBoard.board", GameBoard.board)
-testInput = torch.from_numpy(convertBoard2PowBoard(GameBoard.board))
-res = policyNet(testInput)
-print("res", res)
-print("torch.max(res, 1)[1].data.numpy()", torch.max(res, 1)[1].data.numpy())
+# GameBoard2 = Board(4)
+# GameBoard2.printBoard()
+# GameBoard2.moveLeft()
+# GameBoard2.printBoard()
+# GameBoard2.addNewTile()
+# GameBoard2.printBoard()
+# GameBoard2.moveUp()
+# GameBoard2.printBoard()
+# GameBoard2.addNewTile()
+# GameBoard2.printBoard()
+
+# print("GameBoard2.board", GameBoard2.board)
+# print("np.stack(GameBoard.board, GameBoard2.board)", np.stack((GameBoard.board, GameBoard2.board)))
+# testInput = convertBoard2PowBoard(torch.from_numpy(np.stack((GameBoard.board, GameBoard2.board))))
+# res = policyNet(testInput)
+# print("res", res)
+# print("torch.max(res, 1)[1].data.numpy()", torch.max(res, 1)[1].data.numpy())
 
 # Hyper Parameters
 LR = 5e-4 # learning rate
 GAMMA = 0.9 # reward discount
 EPSILON = 0.9 # for epsilon greedy algorithm
-REPLAY_MEMORY, REPLAY_LABELS = [], []
+# REPLAY_MEMORY, REPLAY_LABELS = [], []
 
+BATCH_SIZE = 512
+TARGET_REPLACE_ITER = 100   # target update frequenc
 MEMORY_CAPACITY = 6000
 
-loss = []
-scores = []
-final_parameters = {}
 num_episodes = 20000
 
-
 # for episode with max score
-maximum = -1
-episode = -1
+max_episode = -1
+max_score = -1
 
 # total iterations 
 total_iters = 1
 
-# # number of back props
-# back = 0
+# total scores
+scores = []
 
-# tensor = torch.ones(())
-# single_dataset = tensor.new_empty((1, 16, 4, 4), dtype=torch.float32) # tf.placeholder(tf.float32,shape=(1,4,4,16))
-# print("single_dataset", single_dataset)
-# single_output = policy(single_dataset)
-
-
-## 改好这部分
 class DQN:
     def __init__(self):
         self.eval_net, self.target_net = policyNet, policyNet
 
         self.learn_step_counter = 0                                     # for target updating
         self.memory_counter = 0                                         # for storing memory
-        self.memory = np.zeros((MEMORY_CAPACITY, N_STATES * 2 + 2))     # initialize memory
+        self.memory = np.zeros((MEMORY_CAPACITY, inputSize*2+2))        # initialize memory
         self.optimizer = torch.optim.Adam(self.eval_net.parameters(), lr=LR)
         self.loss_func = nn.MSELoss()
 
     def chooseAction(self, GameBoard):
-        inputState = torch.from_numpy(convertBoard2PowBoard(GameBoard.board))
+        inputState = convertBoard2PowBoard(torch.from_numpy(np.expand_dims(GameBoard.board, axis=0)))
         # input only one sample
+        legalMoves = getLegalMoves(GameBoard)
         if np.random.uniform() <= EPSILON:   # greedy
             actionsQvalue = self.eval_net(inputState)
+            # action should first be lgeal actions
+            illegalMask = list(set([0, 1, 2, 3]).difference(set(legalMoves)))
+            for i in illegalMask: actionsQvalue[0][i] = float('-inf')
+            # print("actionsQvalue: ", actionsQvalue)
             action = torch.max(actionsQvalue, 1)[1].data.numpy()[0] # action with maximum Q value
+            # action = # action should be legal
         else:   # random
-            action = np.random.randint(0, 4)
+            action = random.choice(legalMoves)
         return action
 
-    def performAction(self, GameBoard, action):
-        if action == 0: GameBoard.moveUp()
-        elif action == 1: GameBoard.moveDown()
-        elif action == 2: GameBoard.moveLeft()
-        elif action == 3: GameBoard.moveRight()
-        else: assert(False) # should not reach here!
-
-    def store_transition(self, s, a, r, s_):
-        transition = np.hstack((s, [a, r], s_))
+    def store_transition(self, GameBoard, action, reward, newGameBoard):
+        transition = np.hstack((np.array(GameBoard.board).flatten(), [action, reward], np.array(newGameBoard.board).flatten()))
+        # print("transition: ", transition)
         # replace the old memory with new memory
         index = self.memory_counter % MEMORY_CAPACITY
         self.memory[index, :] = transition
@@ -201,126 +208,167 @@ class DQN:
         # sample batch transitions
         sample_index = np.random.choice(MEMORY_CAPACITY, BATCH_SIZE)
         b_memory = self.memory[sample_index, :]
-        b_s = torch.FloatTensor(b_memory[:, :N_STATES])
-        b_a = torch.LongTensor(b_memory[:, N_STATES:N_STATES+1].astype(int))
-        b_r = torch.FloatTensor(b_memory[:, N_STATES+1:N_STATES+2])
-        b_s_ = torch.FloatTensor(b_memory[:, -N_STATES:])
+        b_s = torch.FloatTensor(b_memory[:, :inputSize])
+        b_s = b_s.reshape((BATCH_SIZE, 4, 4)) # reshape back to original 2D board
+        b_a = torch.LongTensor(b_memory[:, inputSize:inputSize+1].astype(int))
+        b_r = torch.FloatTensor(b_memory[:, inputSize+1:inputSize+2])
+        b_s_ = torch.FloatTensor(b_memory[:, -inputSize:])
+        b_s_ = b_s_.reshape((BATCH_SIZE, 4, 4)) # reshape back to original 2D board
 
         # q_eval w.r.t the action in experience
-        q_eval = self.eval_net(b_s).gather(1, b_a)  # shape (batch, 1)
-        q_next = self.target_net(b_s_).detach()     # detach from graph, don't backpropagate
+        q_eval = self.eval_net(convertBoard2PowBoard(b_s)).gather(1, b_a)  # shape (batch, 1)
+        q_next = self.target_net(convertBoard2PowBoard(b_s_)).detach()     # detach from graph, don't backpropagate
         q_target = b_r + GAMMA * q_next.max(1)[0].view(BATCH_SIZE, 1)   # shape (batch, 1)
         loss = self.loss_func(q_eval, q_target)
-
+        # print("loss", loss.item())
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
 
 dqn = DQN()
 
-#读懂code在干啥再改写
+def getLegalMoves(GameBoard):
+    originalScore = GameBoard.score
+    boardU = copy.deepcopy(GameBoard.board)
+    boardD = copy.deepcopy(GameBoard.board)
+    boardL = copy.deepcopy(GameBoard.board)
+    boardR = copy.deepcopy(GameBoard.board)
+
+    # legal actions for player is represented by numbers:
+    # 0 : Up
+    # 1 : Down
+    # 2 : Left
+    # 3 : Right
+    legalMoves = []
+
+    canMoveUp = GameBoard.moveUp()
+    GameBoard.score = originalScore      
+    GameBoard.board = boardU
+    if canMoveUp : legalMoves.append(0)
+
+    canMoveDown = GameBoard.moveDown()
+    GameBoard.score = originalScore    
+    GameBoard.board = boardD
+    if canMoveDown : legalMoves.append(1)
+
+    canMoveLeft = GameBoard.moveLeft()
+    GameBoard.score = originalScore      
+    GameBoard.board = boardL
+    if canMoveLeft : legalMoves.append(2)
+
+    canMoveRight = GameBoard.moveRight()
+    GameBoard.score = originalScore
+    GameBoard.board = boardR
+    if canMoveRight : legalMoves.append(3)
+
+    return legalMoves
+
+def step(GameBoard, action):
+    originalBoard = copy.deepcopy(GameBoard.board)
+    originalScore = GameBoard.score
+    if action == 0: GameBoard.moveUp()
+    elif action == 1: GameBoard.moveDown()
+    elif action == 2: GameBoard.moveLeft()
+    elif action == 3: GameBoard.moveRight()
+    else: assert(False) # should not reach here!
+
+    newBoard = GameBoard.board
+    # Current Reward = number of merges + 0.1*log2(new maximum tile number)
+    mergeNum = findEmptyTiles(newBoard)-findEmptyTiles(originalBoard)
+    GameBoard.addNewTile()
+    # GameBoard.printBoard()
+    newBoard = GameBoard.board
+    maxTile = np.max(newBoard)
+    reward = mergeNum+0.1*math.log2(maxTile)
+    # print("GameBoard.GameOver()", GameBoard.GameOver())
+    if GameBoard.GameOver(): done = True # need further checking
+    else: done = False
+    moveScore = GameBoard.score - originalScore
+    return GameBoard, reward, done, moveScore
+
+# # unit test on step()
+# GameBoard = Board(4)
+# GameBoard.printBoard()
+# GameBoard.moveLeft()
+# GameBoard.addNewTile()
+# GameBoard.printBoard()
+# GameBoard.moveRight()
+# GameBoard.addNewTile()
+# GameBoard.printBoard()
+# GameBoard.moveUp()
+# GameBoard.addNewTile()
+# GameBoard.printBoard()
+# GameBoard.moveRight()
+# GameBoard.addNewTile()
+# GameBoard.printBoard()
+# GameBoard, reward, done, moveScore = step(GameBoard, 2) # move left
+# GameBoard.printBoard()
+# print("reward: ", reward)
+# print("done: ", done)
+# print("moveScore: ", moveScore)
+
+## training 
+print('\nCollecting experience...')
 for episode in range(num_episodes):
     # start state
     GameBoard = Board(4)
     GameBoard.addNewTile()
-    
+    episode_reward = 0
+
     # whether current episode finished or not
-    finish = False
+    done = False
     
     # total score of this episode
-    total_score = 0
+    totalScore = 0
     
-    while(not finish):
+    while(1):
         # choose action
-        action = dqn.chooseAction(s)
+        action = dqn.chooseAction(GameBoard)
 
         # take action
-        # (continue here)
+        newGameBoard, reward, done, moveScore = step(GameBoard, action)
 
-        ## s_, r, done, info = env.step(a)
+        # score of the corresponding action
+        totalScore += moveScore
         
         # decrease the epsilon value during training
         if (episode > 10000) or (EPSILON > 0.1 and total_iters%2500==0):
             EPSILON = EPSILON/1.005
-            
        
-        #change the matrix values and store them in memory
-        prev_state = deepcopy(originalBoard)
-        prev_state = change_values(prev_state)
-        prev_state = np.array(prev_state,dtype=np.float32).reshape(1,4,4,16)
-        replay_labels.append(labels)
-        replay_memory.append(prev_state)
-        
-        
-        #back-propagation
-        if(len(replay_memory)>=mem_capacity):
-            back_loss = 0
-            batch_num = 0
-            z = list(zip(replay_memory,replay_labels))
-            np.random.shuffle(z)
-            np.random.shuffle(z)
-            replay_memory,replay_labels = zip(*z)
-            
-            for i in range(0,len(replay_memory),batch_size):
-                if(i + batch_size>len(replay_memory)):
-                    break
-                    
-                batch_data = deepcopy(replay_memory[i:i+batch_size])
-                batch_labels = deepcopy(replay_labels[i:i+batch_size])
-                
-                batch_data = np.array(batch_data,dtype=np.float32).reshape(batch_size,4,4,16)
-                batch_labels = np.array(batch_labels,dtype=np.float32).reshape(batch_size,output_units)
-            
-                feed_dict = {tf_batch_dataset: batch_data, tf_batch_labels: batch_labels}
-                _,l = session.run([optimizer,loss],feed_dict=feed_dict)
-                back_loss += l 
-                
-                print("Mini-Batch - {} Back-Prop : {}, Loss : {}".format(batch_num,back,l))
-                batch_num +=1
-            back_loss /= batch_num
-            J.append(back_loss)
-            
-            #store the parameters in a dictionary
-            final_parameters['conv1_layer1_weights'] = session.run(conv1_layer1_weights)
-            final_parameters['conv1_layer2_weights'] = session.run(conv1_layer2_weights)
-            final_parameters['conv2_layer1_weights'] = session.run(conv2_layer1_weights)
-            final_parameters['conv2_layer2_weights'] = session.run(conv2_layer2_weights)
-            final_parameters['conv1_layer1_biases'] = session.run(conv1_layer1_biases)
-            final_parameters['conv1_layer2_biases'] = session.run(conv1_layer2_biases)
-            final_parameters['conv2_layer1_biases'] = session.run(conv2_layer1_biases)
-            final_parameters['conv2_layer2_biases'] = session.run(conv2_layer2_biases)
-            final_parameters['fc_layer1_weights'] = session.run(fc_layer1_weights)
-            final_parameters['fc_layer2_weights'] = session.run(fc_layer2_weights)
-            final_parameters['fc_layer1_biases'] = session.run(fc_layer1_biases)
-            final_parameters['fc_layer2_biases'] = session.run(fc_layer2_biases)
-            
-            #number of back-props
-            back+=1
-            
-            #make new memory 
-            replay_memory = list()
-            replay_labels = list()
-            
-        
-        if(local_iters%400==0):
-            print("Episode : {}, Score : {}, Iters : {}, Finish : {}".format(ep,total_score,local_iters,finish))
-        
-        local_iters += 1
-        total_iters += 1
-        
-    scores.append(total_score)
-    print("Episode {} finished with score {}, result : {} board : {}, epsilon  : {}, learning rate : {} ".format(ep,total_score,finish,board,epsilon,session.run(learning_rate)))
-    print()
-    
-    if((ep+1)%1000==0):
-        print("Maximum Score : {} ,Episode : {}".format(maximum,episode))    
-        print("Loss : {}".format(J[len(J)-1]))
-        print()
-        
-    if(maximum<total_score):
-        maximum = total_score
-        episode = ep
-print("Maximum Score : {} ,Episode : {}".format(maximum,episode))    
+        # store them in memory
+        dqn.store_transition(GameBoard, action, reward, newGameBoard)
 
+        episode_reward += reward
+
+        if dqn.memory_counter > MEMORY_CAPACITY:
+            print("Learning new experience...")
+            dqn.learn()
+            if done:
+                print('episode: ', episode,
+                      '| episode_reward: ', round(episode_reward, 2))
+
+        if done: break
+
+        GameBoard = newGameBoard
+        
+    scores.append(totalScore)
+    print("Episode {} finished with score {}, epsilon : {}, learning rate : {} ".format(episode, totalScore, EPSILON, LR))
+    GameBoard.printBoard()
+    
+    if not (episode+1) % 1000:
+        print("Maximum Score : {} ,Episode : {}".format(max_score, max_episode))    
+        print("\n")
+        
+    if max_score < totalScore:
+        max_score = totalScore
+        max_episode = episode
+
+print("Scores: ", scores)
+print("Maximum Score : {} ,Episode : {}".format(max_score, max_episode))    
+
+def save_policy(policyNet):
+    torch.save(policyNet.state_dict(), "mypolicy.pth")
+
+save_policy(policyNet)
 
 
