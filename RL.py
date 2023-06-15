71a2604f7fbc4c28aa179dfa4da283d39f9c0d17
CanYouTeachMeHowToCode <yilunw1030@gmail.com>
2023-06-15 15:04:51 -0400

    ignore UI.py in linting solved the problem..probably sth wrong with the tkinter for linting

--- RL.py
+++ RL.py
@@ -1,16 +1,16 @@
 ## Deep Reinforcement Learning part
 
-import random
-import copy
-import math
+# import random
+# import copy
+# import math
 
-import torch
-from torch import optim
-from torch import nn
-import torch.nn.functional as F
-import numpy as np
+# import torch
+# from torch import optim
+# from torch import nn
+# import torch.nn.functional as F
+# import numpy as np
 
-from board import Board
+# from board import Board
 
 """
 2023.06.14 update: Empty all the stuff and start from scratch based on another approach
